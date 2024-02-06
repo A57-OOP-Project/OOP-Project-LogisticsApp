6a69893f016349f5e63b80abd6848c2b5d4802f1
Mihail Radoev <mihailiradoev@gmail.com>
2024-02-06 17:07:06 +0200

    Commit1

--- models/package.py
+++ models/package.py
@@ -1,10 +1,38 @@
 class Package:
-    '''
-    Olesya and Svetlio think that we should have expected_arrival_time attribute with getter and setter 
-    '''
     def __init__(self, id, start_location, end_location, weight, contact_info):
-       #self._id = id
-        pass
+        self._id = id
+        self._start_location = start_location
+        self._end_location = end_location
+        self._weight = weight
+        self._contact_info = contact_info
+        self._expected_arrival_time = None
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def start_location(self):
+        return self._start_location
+
+    @property
+    def end_location(self):
+        return self._end_location
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @property
+    def contact_info(self):
+        return self._contact_info
+
+    @property
+    def get_expected_arrival_time(self):
+        return self._expected_arrival_time
+
+    def set_expected_arrival_time(self, expected_arrival_time):
+        self._expected_arrival_time = expected_arrival_time
 
     def info(self):
-        pass
+        return f"Package ID: {self._id}\nStart Location: {self._start_location}\nEnd Location: {self._end_location}\nWeight: {self._weight}\nContact Info: {self._contact_info}\nExpected Arrival Time: {self._expected_arrival_time}"
