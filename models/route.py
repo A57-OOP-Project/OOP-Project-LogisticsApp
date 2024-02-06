7baddfd4d8c3b99d7d48c0112a2878cb6ae4e006
svmoll <156302253+svmoll@users.noreply.github.com>
2024-02-06 16:29:34 +0200

    models.route latest

--- models/route.py
+++ models/route.py
@@ -7,8 +7,8 @@ class Route:
         self._id = id 
         self._packages = []
         self._trucks = []
-        # self._locations: list[tuple[str, datetime]] = [] #  [(location_1, departure_time), (location_2), arrival_time)]
-        self._locations = locations
+        # self._locations: list[tuple[str, datetime]] = [] #  [(location_1, departure_time), (location_2, estimated_arrival_time)]
+        self._locations: list[tuple[str, datetime]] = locations
 
     @property
     def id(self):
@@ -18,22 +18,22 @@ class Route:
     def locations(self):
         return self._locations
     
-    @locations.setter
-    def locations(self,value):
-        self.ensure_locations_is_valid(value)
-        self._locations.append(value)
-        print(value)
-        return self._locations
+    # @locations.setter
+    # def locations(self,value):
+    #     self.ensure_locations_is_valid(value)
+    #     self._locations.append(value)
+    #     print(value)
+    #     return self._locations
 
     def ensure_locations_is_valid(self,locations):
-        if (
+        if  (
             isinstance(locations, tuple) and 
             len(locations) == 2 and 
             isinstance(locations[0], (str, int)) and 
             isinstance(locations[1], datetime)
             ):
             return True
-        raise ValueError('Invalid location format')
+        raise ValueError('Invalid locations format')
 
     def calculate_route_distance(self):
         '''Depends on constants.distances when the matrix is ready'''
@@ -52,22 +52,29 @@ class Route:
     def __str__(self) -> str:
         stops: list = []
         next_stop = ''
-        # Check if route is in progress/active
-        if len(self._packages) > 0 and len(self._trucks) > 0:
+        if len(self._packages) > 0 and len(self._trucks) > 0: # Check if route is in progress/active
             pass
-        #Add all the stops. Should they be all or remaining?
-        for key in self._locations:
-            stops.append(key[0])
-                # #Add total_delivery_weight calc - is ._weight ready in models.package? import models.Package once ready. Create a test.
-                # delivery_weight: int = 0
-                # for i in self._packages:
-                #     delivery_weight += i._weight
+        for location in self._locations:  #Add all the stops. Should they be all or remaining?
+            try: 
+                if type(location) == str:
+                    stops.append(location)
+            except:
+                pass
+        # #Add total_delivery_weight calc - is ._weight ready in models.package? import models.Package once ready. Create a test.
+        # delivery_weight: int = 0
+        # for i in self._packages:
+        #     delivery_weight += i._weight
+        
         #Add next stop based on time of day
         time_now = datetime.now(tz=None) 
         for arrival_time_at_stop in self._locations:
-            if arrival_time_at_stop > time_now:
-                next_stop = arrival_time_at_stop 
-                break
+            try:
+                if type(arrival_time_at_stop) == datetime:
+                    if arrival_time_at_stop > time_now:
+                        next_stop = self._locations[self._locations.index(arrival_time_at_stop)-1]
+                        break
+            except:
+                pass
         return '\n'.join([
             f'Delivery stops left: {stops}',
             f'Delivery weight: delivery weight to be added',
@@ -78,6 +85,6 @@ class Route:
         #What is this def for? 
         pass
 
-route = Route(id=1, locations=("Location1", datetime(2024, 2, 5, 10, 30)))
-print(route.locations)
-print(route.__str__())
+# Check
+# route = Route(id=1, locations=("Location1", datetime(2025, 2, 5, 10, 30)))
+# print(route.__str__())
