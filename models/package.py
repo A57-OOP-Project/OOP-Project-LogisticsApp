d413c2a6e60f36bd692ef138dec5716abdec2756
Mihail Radoev <mihailiradoev@gmail.com>
2024-02-11 16:45:20 +0200

    Customer class Package composition

--- models/package.py
+++ models/package.py
@@ -1,10 +1,12 @@
+from customer import Customer
+
 class Package:
-    def __init__(self, id, start_location, end_location, weight, contact_info):
+    def __init__(self, id, start_location, end_location, weight, customer):
         self._id = id
         self._start_location = start_location
         self._end_location = end_location
         self._weight = weight
-        self._contact_info = contact_info
+        self._customer = customer
         self._expected_arrival_time = None
 
     @property
@@ -24,9 +26,8 @@ class Package:
         return self._weight
 
     @property
-    def contact_info(self):
-        return self._contact_info
-
+    def customer(self):
+        return self._customer
     @property
     def get_expected_arrival_time(self):
         return self._expected_arrival_time
@@ -35,4 +36,12 @@ class Package:
         self._expected_arrival_time = expected_arrival_time
         
     def info(self):
-        return f"Package ID: {self._id}\nStart Location: {self._start_location}\nEnd Location: {self._end_location}\nWeight: {self._weight}\nContact Info: {self._contact_info}\nExpected Arrival Time: {self._expected_arrival_time}"
+        return f"Package ID: {self._id}\nStart Location: {self._start_location}\nEnd Location: {self._end_location}\nWeight: {self._weight}\nCustomer Info:\nName: {self._customer._name}\nTelephone: {self._customer._telephone}\nExpected Arrival Time: {self._expected_arrival_time}"
+
+
+#customer = Customer("Alice", "123-456-7890")
+
+#package = Package("PKG001", "New York", "Los Angeles", 10, customer)
+#package.expected_arrival_time = "2024-02-15 12:00"
+
+#print(package.info())
