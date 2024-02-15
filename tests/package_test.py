a4795c588e3df048162b926cffaa55ba224d3840
Mihail Radoev <mihailiradoev@gmail.com>
2024-02-15 13:52:27 +0200

    Customer class Package composition

---
+++ tests/package_test.py
@@ -0,0 +1,35 @@
+import unittest
+from models.package import Package
+
+
+class PackageShould(unittest.TestCase):
+    def setUp(self):
+        self.package = Package(
+            id=123,
+            start_location="SYD",
+            end_location="MEL",
+            weight=5000,
+            contact_info="Pesho"
+        )
+
+    def test_properties(self):
+        self.assertEqual(self.package.id, 123)
+        self.assertEqual(self.package.start_location, "SYD")
+        self.assertEqual(self.package.end_location, "MEL")
+        self.assertEqual(self.package.weight, 5000)
+        self.assertEqual(self.package.contact_info, "Pesho")
+        self.assertIsNone(self.package.expected_arrival_time)
+
+    def test_expected_arrival_time(self):
+        self.package.expected_arrival_time = "2024-02-14 12:00"
+        self.assertEqual(self.package.expected_arrival_time, "2024-02-14 12:00")
+
+    def test_info(self):
+        expected_info = "Package ID: 123\n" \
+                        "Start Location: SYD\n" \
+                        "End Location: MEL\n" \
+                        "Weight: 5000 kg\n" \
+                        "Contact Info: Pesho\n" \
+                        "Expected Arrival Time: 2024-02-14 12:00"
+        self.package.expected_arrival_time = "2024-02-14 12:00"
+        self.assertEqual(self.package.info(), expected_info)
