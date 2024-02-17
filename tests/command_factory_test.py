50377db0261d9bcedc4f04de7ac872e4074f2128
olesya-karpets <olesya.karpec@gmail.com>
2024-02-17 23:22:04 +0200

    New: command_factory_test.py

--- tests/command_factory_test.py
+++ tests/command_factory_test.py
@@ -26,8 +26,172 @@ def test_setup():
 class Create_Should(TestCase):
     
     def test_raiseError_invalidCommandName(self):
-        # Arrange
+        
         input_line = 'asd 1 2 3'
         cmd_factory, app_data = test_setup()
+        
+        with self.assertRaises(InvalidCommand):
+            command = cmd_factory.create(input_line)
+        
+        
+    def test_createPackageCommand_withCorrectParams(self):
+       
+        input_line = 'createpackage MEL SYD 1000 BaiGanio,phone:0888123444'
+        cmd_factory, app_data = test_setup()
 
-        #to be continued :)
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, CreatePackage)
+        self.assertIsInstance(command._models_factory, ModelsFactory)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('MEL', 'SYD', '1000', 'BaiGanio,phone:0888123444'), command.params)
+        
+    def test_createRouteCommand_withCorrectParams(self):
+       
+        input_line = 'createroute MEL->SYD->BRI 2024-02-18 14:30'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, CreateRoute)
+        self.assertIsInstance(command._models_factory, ModelsFactory)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('MEL->SYD->BRI', '2024-02-18', '14:30'), command.params)
+        
+    def test_createViewNotAssignedCommand_withCorrectParams(self):
+       
+        input_line = 'viewnotassigned'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, ViewNotAssigned)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual((), command.params)
+        
+    def test_createAddPackageCommand_withCorrectParams(self):
+       
+        input_line = 'addpackage 1 1'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, AddPackage)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('1', '1'), command.params)
+        
+        
+    def test_createViewPackageCommand_withCorrectParams(self):
+       
+        input_line = 'viewpackage 1'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, ViewPackage)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('1',), command.params)
+        
+        
+    def test_createAddTruckCommand_withCorrectParams(self):
+       
+        input_line = 'addtruck scania 1001 1'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, AddTruck)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('scania', '1001', '1'), command.params)
+        
+        
+    def test_createViewRouteCommand_withCorrectParams(self):
+       
+        input_line = 'viewroute 1'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, ViewRoute)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('1',), command.params)
+        
+    def test_createFindRouteCommand_withCorrectParams(self):
+       
+        input_line = 'findroute ASP MEL'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, FindRoute)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('ASP', 'MEL'), command.params)
+        
+    def test_createCheckRouteCapacityCommand_withCorrectParams(self):
+       
+        input_line = 'checkroutecapacity 1 MEL SYD 1000'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, CheckRouteCapacity)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('1', 'MEL', 'SYD', '1000'), command.params)
+        
+    def test_createRemovePackageFromRouteCommand_withCorrectParams(self):
+       
+        input_line = 'removepackagefromroute 1 1'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, RemovePackageFromRoute)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('1', '1'), command.params)
+        
+    def test_createFindTruckCommand_withCorrectParams(self):
+       
+        input_line = 'findtruck 1000 MEL SYD BRI'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, FindTruck)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('1000', 'MEL', 'SYD', 'BRI'), command.params)
+        
+    def test_createViewSystemCommand_withCorrectParams(self):
+       
+        input_line = 'viewsystem 0'
+        cmd_factory, app_data = test_setup()
+
+        command = cmd_factory.create(input_line)
+
+        self.assertIsInstance(command, ViewSystem)
+        self.assertEqual(app_data, command.app_data)
+        self.assertEqual(('0',), command.params)
+        
+    
+      
+        
+    
+        
+        
+    
+    
+        
+    
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        
+    
+    
+
+    
